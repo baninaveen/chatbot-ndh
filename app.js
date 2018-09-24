6421c6db6b2181bd44261dce42b6b328771b4a98
naveen <kumar.naveen@senrysa.com>
2018-09-24 12:53:46 +0530

    Fsdfd

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 			fetch('http://blog.nextdoorhub.com/wordpress/wp-json/wp/v2/posts/')
 				.then( r => r.json())
 				.then(data => {
-					console.log(data[1]['title']);
+					console.log(data[1]['title']['rendered']);
 				});
 			break;	
 		default:
