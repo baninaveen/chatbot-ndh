ba19788d4dfc767c52aaf31008ed9efbae377816
naveen <kumar.naveen@senrysa.com>
2018-09-24 12:16:49 +0530

    fdsfd

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 		case "BlogActivity":
 			axios.get('http://blog.nextdoorhub.com/wordpress/wp-json/wp/v2/posts/')
 				.then((res) => {
-					console.log('res', res[0].title.rendered.text);
+					console.log('res', res.title.rendered.text);
 				})
 				.catch((err)=>{
 					console.log('Error in BlogActivity', err);
