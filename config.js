0adf5da1ff740c02101b85401bb03e9d8c5a850a
naveen <kumar.naveen@senrysa.com>
2018-09-24 11:12:54 +0530

    uosfujdfof

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAADkX01wZBhcBALOHP3uim2ZAYOq3tL3gVtAxiMGZAAEeV1fjik0hTdSCaME87i8r6OWyKg8eKbLsGUlTcgeBkhJoWFYmiH45uRTZCgXkrVJEbvS8JbWKTIKdqoZBC7GrEZA85ZCTVZACzj0IgbrCXSwRdbbyNy3S9R48fe5NBtvrQZDZD',
+    FB_VERIFY_TOKEN: 'ndh_chatbot',
+    API_AI_CLIENT_ACCESS_TOKEN: '5a725d1112c746d7a72efb7a15f3e12a',
+    FB_APP_SECRET: '56fd35311eceb90f1119b6944481822c',
+    SERVER_URL: "https://ndh-chatbot-facebook.herokuapp.com/",
 };
