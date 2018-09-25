23792d938e717e9e2f53dbd01da0c2280a27c2e8
naveen <kumar.naveen@senrysa.com>
2018-09-25 13:12:27 +0530

    upate

--- FbResponses.js
+++ FbResponses.js
@@ -1,6 +1,7 @@
 const config = require('./config');
 const request = require('request');
 
+// Internal Functions
 function callSendAPI(messageData) {
 	request({
 		uri: 'https://graph.facebook.com/v2.6/me/messages',
@@ -27,6 +28,17 @@ function callSendAPI(messageData) {
 		}
 	});
 }
+
+function isDefined(obj) {
+	if (typeof obj == 'undefined') {
+		return false;
+	}
+	if (!obj) {
+		return false;
+	}
+	return obj != null;
+}
+
 
 module.exports = {
     
@@ -318,34 +330,5 @@ module.exports = {
         };
 
         callSendAPI(messageData);
-    },
-
-
-    // Call Facebook Send API 
-    callSendAPI: (messagedata) => {
-        request({
-            uri: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {
-                access_token: config.FB_PAGE_TOKEN
-            },
-            method: 'POST',
-            json: messageData
-    
-        }, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var recipientId = body.recipient_id;
-                var messageId = body.message_id;
-    
-                if (messageId) {
-                    console.log("Successfully sent message with id %s to recipient %s",
-                        messageId, recipientId);
-                } else {
-                    console.log("Successfully called Send API for recipient %s",
-                        recipientId);
-                }
-            } else {
-                console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
-            }
-        });
     }
 }
