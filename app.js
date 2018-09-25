7ce59beade5e3e07375a5a009fb47bc41943ba17
naveen <kumar.naveen@senrysa.com>
2018-09-25 16:29:03 +0530

    bug in sendapiai

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const fbAction = require('./FbActions');
 const FbPostbacks = require('./FbPostbacks');
 const FacebookServices = require('./FacebookServices');
 const aiService = require('./apiAiService');
+const fbResponse = require('./FbResponses');
 
 // Messenger API parameters
 if (!config.FB_PAGE_TOKEN) {
@@ -49,6 +50,12 @@ app.use(bodyParser.urlencoded({
 
 // Process application/json
 app.use(bodyParser.json())
+
+const apiAiService = apiai(config.API_AI_CLIENT_ACCESS_TOKEN, {
+	language: "en",
+	requestSource: "fb"
+});
+
 
 // Index route
 app.get('/', function (req, res) {
@@ -90,7 +97,7 @@ app.post('/webhook/', function (req, res) {
 				if (messagingEvent.optin) {
 					FacebookServices.receivedAuthentication(messagingEvent);
 				} else if (messagingEvent.message) {
-					FacebookServices.receivedMessage(messagingEvent);
+					receivedMessage(messagingEvent);
 				} else if (messagingEvent.delivery) {
 					FacebookServices.receivedDeliveryConfirmation(messagingEvent);
 				} else if (messagingEvent.postback) {
@@ -144,10 +151,86 @@ function receivedMessage(event){
 
 	if (messageText) {
 		//send message to api.ai
-		aiService.sendToApiAi(senderID, messageText);
+		sendToApiAi(senderID, messageText);
 	} else if (messageAttachments) {
 		fbAction.handleMessageAttachments(messageAttachments, senderID);
 	}
+}
+
+
+function handleApiAiResponse (sender, response) {
+    let responseText = response.result.fulfillment.speech;
+    let responseData = response.result.fulfillment.data;
+    let messages = response.result.fulfillment.messages;
+    let action = response.result.action;
+    let contexts = response.result.contexts;
+    let parameters = response.result.parameters;
+
+    fbResponse.sendTypingOff(sender);
+
+    if (config.isDefined(messages) && (messages.length == 1 && messages[0].type != 0 || messages.length > 1)) {
+        let timeoutInterval = 1100;
+        let previousType ;
+        let cardTypes = [];
+        let timeout = 0;
+        for (var i = 0; i < messages.length; i++) {
+
+            if ( previousType == 1 && (messages[i].type != 1 || i == messages.length - 1)) {
+
+                timeout = (i - 1) * timeoutInterval;
+                setTimeout(facebookServices.handleCardMessages.bind(null, cardTypes, sender), timeout);
+                cardTypes = [];
+                timeout = i * timeoutInterval;
+                setTimeout(facebookServices.handleMessage.bind(null, messages[i], sender), timeout);
+            } else if ( messages[i].type == 1 && i == messages.length - 1) {
+                cardTypes.push(messages[i]);
+                        timeout = (i - 1) * timeoutInterval;
+                        setTimeout(facebookServices.handleCardMessages.bind(null, cardTypes, sender), timeout);
+                        cardTypes = [];
+            } else if ( messages[i].type == 1 ) {
+                cardTypes.push(messages[i]);
+            } else {
+                timeout = i * timeoutInterval;
+                setTimeout(facebookServices.handleMessage.bind(null, messages[i], sender), timeout);
+            }
+
+            previousType = messages[i].type;
+
+        }
+    } else if (responseText == '' && !config.isDefined(action)) {
+        //api ai could not evaluate input.
+        console.log('Unknown query' + response.result.resolvedQuery);
+        fbResponse.sendTextMessage(sender, "I'm not sure what you want. Can you be more specific?");
+    } else if (isDefined(action)) {
+        // handleApiAiAction(sender, action, responseText, contexts, parameters);
+        fbAction.FacebookAction(sender, action, responseText, contexts, parameters);
+    } else if (config.isDefined(responseData) && config.isDefined(responseData.facebook)) {
+        try {
+            console.log('Response as formatted message' + responseData.facebook);
+            fbResponse.sendTextMessage(sender, responseData.facebook);
+        } catch (err) {
+            fbResponse.sendTextMessage(sender, err.message);
+        }
+    } else if (config.isDefined(responseText)) {
+
+        fbResponse.sendTextMessage(sender, responseText);
+    }
+}
+
+function sendToApiAi(sender, text) {
+	fbResponse.sendTypingOn(sender);
+	let apiaiRequest = apiAiService.textRequest(text, {
+		sessionId: sessionIds.get(sender)
+	});
+
+	apiaiRequest.on('response', (response) => {
+		if (config.isDefined(response.result)) {
+			handleApiAiResponse(sender, response);
+		}
+	});
+
+	apiaiRequest.on('error', (error) => console.error(error));
+	apiaiRequest.end();
 }
 
 // Spin up the server
