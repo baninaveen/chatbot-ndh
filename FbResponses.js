17bdd339a9ff043720809c4c4780be4a33a08c2a
naveen <kumar.naveen@senrysa.com>
2018-09-25 13:08:16 +0530

    yoate

--- FbResponses.js
+++ FbResponses.js
@@ -1,6 +1,32 @@
 const config = require('./config');
 const request = require('request');
 
+function callSendAPI(messageData) {
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {
+			access_token: config.FB_PAGE_TOKEN
+		},
+		method: 'POST',
+		json: messageData
+
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var recipientId = body.recipient_id;
+			var messageId = body.message_id;
+
+			if (messageId) {
+				console.log("Successfully sent message with id %s to recipient %s",
+					messageId, recipientId);
+			} else {
+				console.log("Successfully called Send API for recipient %s",
+					recipientId);
+			}
+		} else {
+			console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+		}
+	});
+}
 
 module.exports = {
     
