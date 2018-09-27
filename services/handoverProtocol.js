37613d257d9314d9644bcd2a1344c15909bdc741
naveen <kumar.naveen@senrysa.com>
2018-09-27 16:54:43 +0530

    upat e

--- services/handoverProtocol.js
+++ services/handoverProtocol.js
@@ -1,32 +1,32 @@
-'use strict';
+// 'use strict';
 
-//import API helper
-const api = require('./api');
+// //import API helper
+// const api = require('./api');
 
-function passThreadControl (userPsid, targetAppId) {
-  console.log('PASSING THREAD CONTROL')
-  let payload = {
-    recipient: {
-      id: userPsid
-    },
-    target_app_id: targetAppId
-  };
+// function passThreadControl (userPsid, targetAppId) {
+//   console.log('PASSING THREAD CONTROL')
+//   let payload = {
+//     recipient: {
+//       id: userPsid
+//     },
+//     target_app_id: targetAppId
+//   };
 
-  api.call('/pass_thread_control', payload, () => {});
-}
+//   api.call('/pass_thread_control', payload, () => {});
+// }
 
-function takeThreadControl (userPsid) {
-  console.log('TAKING THREAD CONTROL')
-  let payload = {
-    recipient: {
-      id: userPsid
-    }
-  };
+// function takeThreadControl (userPsid) {
+//   console.log('TAKING THREAD CONTROL')
+//   let payload = {
+//     recipient: {
+//       id: userPsid
+//     }
+//   };
 
-  api.call('/take_thread_control', payload, () => {});
-}
+//   api.call('/take_thread_control', payload, () => {});
+// }
 
-module.exports = {
-  passThreadControl,
-  takeThreadControl
-};
+// module.exports = {
+//   passThreadControl,
+//   takeThreadControl
+// };
